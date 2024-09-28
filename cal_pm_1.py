35b84c89be1a7ea59e1ac7a2f4f03afdd967edec
yoon0915 <mikooyoon@naver.com>
2024-09-28 16:37:41 +0900

    Initial commit: Plus, Minus functions

---
+++ cal_pm_1.py
@@ -0,0 +1,32 @@
+def cal_plus():
+    int_1 = int(input("첫 번째 숫자를 입력하시오: "))
+    int_2 = int(input("두 번째 숫자를 입력하시오: "))
+    result = int_1 + int_2
+    print(f"{int_1} + {int_2} = {result}")
+
+
+def cal_minus():
+    int_1 = int(input("첫 번째 숫자를 입력하시오: "))
+    int_2 = int(input("두 번째 숫자를 입력하시오: "))
+    result = int_1 - int_2
+    print(f"{int_1} - {int_2} = {result}")
+
+
+
+def calculator():
+    while True:
+        print("덧셈(+), 뺄셈(-) 중 수행할 연산 기호을 입력하세요(종료를 원하면 q 입력)")
+        symbols = input("연산 기호(+,-,)를 입력하세요: ")
+
+        if symbols == '+':
+            cal_plus()
+        elif symbols == '-':
+            cal_minus()
+            cal_div()                        
+        elif symbols == 'q':
+            print("프로그램을 종료합니다.")
+            break
+        else:
+            print("잘못된 연산기호를 입력하였습니다.")
+
+calculator()
