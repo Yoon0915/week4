41915917665f77d576a618e316ffcf569e804e20
yoon0915 <mikooyoon@naver.com>
2024-09-28 16:52:16 +0900

    Second Commit : Add multi and div func

--- cal_pm_1.py
+++ cal_pm_1.py
@@ -11,17 +11,31 @@ def cal_minus():
     result = int_1 - int_2
     print(f"{int_1} - {int_2} = {result}")
 
+def cal_mul():
+    int_1 = int(input("첫 번째 숫자를 입력하시오: "))
+    int_2 = int(input("두 번째 숫자를 입력하시오: "))
+    result = int_1 * int_2
+    print(f"{int_1} x {int_2} = {result}")
+
+def cal_div():
+    int_1 = int(input("첫 번째 숫자를 입력하시오: "))
+    int_2 = int(input("두 번째 숫자를 입력하시오: "))
+    result = int_1 / int_2
+    print(f"{int_1} / {int_2} = {result}")
 
 
 def calculator():
     while True:
-        print("덧셈(+), 뺄셈(-) 중 수행할 연산 기호을 입력하세요(종료를 원하면 q 입력)")
-        symbols = input("연산 기호(+,-,)를 입력하세요: ")
+        print("덧셈(+), 뺄셈(-), 곱셉(*), 나눗셈(/) 중 수행할 연산 기호을 입력하세요(종료를 원하면 q 입력)")
+        symbols = input("연산 기호(+,-,*,/)를 입력하세요: ")
 
         if symbols == '+':
             cal_plus()
         elif symbols == '-':
             cal_minus()
+        elif symbols == '*':
+            cal_mul()
+        elif symbols == '/':
             cal_div()                        
         elif symbols == 'q':
             print("프로그램을 종료합니다.")
